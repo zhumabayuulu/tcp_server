9e9184dfe946bd9fdbd286a789c964cb32d412e9
almanbet <103384218+zhumabayuulu@users.noreply.github.com>
2025-05-24 01:12:03 +0600

    fix

--- lock_server/settings.py
+++ lock_server/settings.py
@@ -54,7 +54,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.getenv('DB_FILE', os.path.join(BASE_DIR, 'new_database.db')),
+        'NAME': os.getenv('DB_FILE', os.path.join(BASE_DIR, 'test_database.db')),
     }
 }
 
